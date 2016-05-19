900ad466f2f2ec04dc6e3496a8fb55594bb451e8
imdira <indiraprofir@yahoo.ro>
2016-05-19 21:08:51 +0100

    Current status with delete item working.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var piataItems = itemsInStorage();
+console.log("You currently have this in storage:" + JSON.stringify(piataItems));
 var piataList = document.getElementById("piataList");
 
 function processNewItem() { 
@@ -20,49 +21,61 @@ function itemsInStorage() {
   else return [];
 }
 
-function getItemIndex(givenItem, list) {
-  var index = 0;
-  var listItems = list.getElementsByTagName("li");
-  for (var i = 0; i < listItems.length; i++) { 
-    var item = listItems[i];
-    if (item.textContent === givenItem) {
-      return index + 1;
-    }
-    else {
-      index ++;
-    }
-  }
-  return -1;
-}
-
-function addListItem(newItem, list) {
-  if (document.getElementById(newItem)) {
-    alert("Alo, baga alteceva, deja ai pus " + newItem + ".");
+function addListItem(givenItem, list) {
+  if (document.getElementById(givenItem)) {
+    alert("Alo, baga alteceva, deja ai pus " + givenItem + ".");
   }
   else {
+    var itemId = Math.random();
     var deleteButton = document.createElement("input");
     deleteButton.type = "button";
     deleteButton.value = "x";
     deleteButton.class = "deleteButton";
-    var newElement = document.createElement("li");
-    newElement.id = list.getElementsByTagName("li").length + 1;
-    var newContent = document.createTextNode(newItem);
-    newElement.appendChild(newContent);
-    newElement.appendChild(deleteButton);
-    list.appendChild(newElement);
+    deleteButton.setAttribute('data-parentId', itemId);
+
+    var saveButton = document.createElement("input");
+    saveButton.type = "button";
+    saveButton.value = "save";
+    saveButton.class = "saveButton";
+    saveButton.setAttribute('data-parentId', itemId);
+
+
+    var listItem = document.createElement("li");
+    var inputItem = document.createElement("input");
+    inputItem.id = itemId;
+    inputItem.value = givenItem;
+    listItem.appendChild(inputItem);
+    listItem.appendChild(deleteButton);
+    listItem.appendChild(saveButton);
+    list.appendChild(listItem);
+
     deleteButton.addEventListener("click", function() {
-      removeItem(newItem, list);
+      removeItem(this, saveButton, givenItem, list);
+    });
+
+    inputItem.addEventListener("input", function(){});
+
+    saveButton.addEventListener("click", function() {
+      var i = piataItems.indexOf(givenItem);
+      piataItems[i] = inputItem.value;
+      console.log("Replaced " + piataItems[i] + " with " + inputItem.value);
     });
   }
 };
 
-function removeItem(givenItem, list) {
+function updateItem(saveButton, givenItem, list) {
+
+};
+
+function removeItem(deleteButton, saveButton, givenItem, list) {
   var index = piataItems.indexOf(givenItem);
   if (index > -1) {
     piataItems.splice(index, 1);
   }
   localStorage.setItem('items', JSON.stringify(piataItems));
-  document.getElementById(getItemIndex(givenItem, list)).remove();
+  document.getElementById(deleteButton.getAttribute("data-parentId")).remove();
+  deleteButton.remove();
+  saveButton.remove();
 };
 
 function init(){
